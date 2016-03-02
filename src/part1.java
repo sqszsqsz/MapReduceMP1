fa2c94ef37d7b8ac4d26e9e648a2ceeece8856c8
sqszsqsz <sqszsqsz@hotmail.com>
2016-03-02 16:42:47 -0600

    updated task1

--- src/part1.java
+++ src/part1.java
@@ -2,17 +2,22 @@
  * Created by SweetSoul on 3/2/2016.
  */
 
-//use winSCP to import files into hue
+// use winSCP to import files into hue
+
+// Basic MR code was modified from the online tutorial
+// http://hortonworks.com/wp-content/uploads/2012/03/Tutorial_Hadoop_HDFS_MapReduce.pdf
+// and https://www.youtube.com/watch?v=4fzhAmnWlrg&feature=youtu.be
 
 import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -23,32 +28,51 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class part1 extends Configured implements Tool{
 
-    public static class Map extends Mapper<Object, Text, Text, IntWritable>
+
+
+    public static class Map extends Mapper<Object, Text, Text, MapWritable>
     {
-        private final static IntWritable one = new IntWritable(1);
+        MapWritable map_out = new MapWritable();
+        Text textKey = new Text();
 
         @Override
-        public void map(Object key, Text value, org.apache.hadoop.mapreduce.Mapper.Context output) throws IOException,
-                InterruptedException {
+        public void map(Object key, Text value, org.apache.hadoop.mapreduce.Mapper.Context output) throws IOException, InterruptedException {
 
-            //If more than one word is present, split using white space.
-            String[] words = value.toString().split(" ");
-            //Only the first word is the candidate name
-            output.write(new Text(words[0]), one);
+            String[] words = value.toString().split(",");
+            map_out.put(new IntWritable(1),new IntWritable(Integer.parseInt(words[2])));
+            map_out.put(new IntWritable(2),new IntWritable(Integer.parseInt(words[3])));
+            map_out.put(new IntWritable(3),new IntWritable(Integer.parseInt(words[4])));
+            textKey.set(words[5]);
+
+            System.out.println(words[0]+' '+words[1]+' '+words[2]+' '+words[3]+' '+words[4]+' '+words[5]);
+
+            output.write(textKey,map_out);
         }
 
     }
 
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>
+    public static class Reduce extends Reducer<Text, MapWritable, Text, Text>
     {
+        Text reduce_out = new Text();
+
         @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context output)
-                throws IOException, InterruptedException {
-            int voteCount = 0;
-            for (IntWritable value : values) {
-                voteCount += value.get();
+        public void reduce(Text key, Iterable<MapWritable> values, Context output) throws IOException, InterruptedException {
+
+            int impression = 0, click = 0, conversion = 0;
+
+            for (MapWritable value : values) {
+                int i = ((IntWritable) value.get(new IntWritable(1))).get();
+                int j = ((IntWritable) value.get(new IntWritable(2))).get();
+                int k = ((IntWritable) value.get(new IntWritable(3))).get();
+
+                impression = impression+i;
+                click = click + j;
+                conversion = conversion + k;
             }
-            output.write(key, new IntWritable(voteCount));
+
+            reduce_out.set(impression+"\t"+click+"\t"+conversion);
+            System.out.println(reduce_out);
+            output.write(key,reduce_out);
         }
 
     }
@@ -64,13 +88,13 @@ public class part1 extends Configured implements Tool{
     @Override
     public int run(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.println("usage: [input] [output]");
+            System.out.println("Input: yarn jar package.jar input output");
             System.exit(-1);
         }
 
         Job job = Job.getInstance(new Configuration());
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
 
         job.setMapperClass(Map.class);
         job.setReducerClass(Reduce.class);
@@ -78,7 +102,9 @@ public class part1 extends Configured implements Tool{
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
+        //File Input argument passed as a command line argument
         FileInputFormat.setInputPaths(job, new Path(args[0]));
+        //File Output argument passed as a command line argument
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.setJarByClass(part1.class);
